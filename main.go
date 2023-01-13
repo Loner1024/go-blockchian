63258daa766a8396cdd1244a52d577133c21c03c
Loner1024 <xth12138@gmail.com>
2023-01-14 00:46:15 +0800

    feat:test prototype

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"github.com/Loner1024/go-blockchain/chain"
+)
+
+func main() {
+	blockChain := chain.NewBlockChain()
+	blockChain.AddBlock("Send 1 Btc to Ivan")
+	blockChain.AddBlock("Send 2 Btc to Ivan")
+	
+	for _, block := range blockChain.Blocks {
+		fmt.Printf("Prev hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
