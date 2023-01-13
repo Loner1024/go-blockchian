8e2bc9df2c7379c6b2ddf9702067a714a9696f22
Loner1024 <xth12138@gmail.com>
2023-01-14 00:37:46 +0800

    feat:block

---
+++ block/block.go
@@ -0,0 +1,37 @@
+// package block is about block.
+package block
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+// Block is Block.
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+// NewBlock create a new block.
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Hash:          nil,
+	}
+	block.SetHash()
+	return block
+}
+
+// SetHash will set hash value for block.
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
